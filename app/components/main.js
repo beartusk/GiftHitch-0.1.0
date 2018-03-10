cb2b0b274cd9a5f192a89758829cd2d000d9069f
Ubuntu <ubuntu@ip-172-31-47-84.us-east-2.compute.internal>
2018-03-10 20:00:49 +0000

    first commit

---
+++ app/components/main.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Panel, Col} from 'react-bootstrap'
+function Begin () {
+  return (
+    <div style={{backgroundColor: "black"}}>
+    <Col sm={12} md={6}>
+      <Panel bsStyle="primary" style={{height: 500}}>
+    	<Panel.Heading>
+      	  <Panel.Title componentClass="h3">New Beginnings</Panel.Title>
+    	</Panel.Heading>
+       	<Panel.Body>This will be a website that displays the working portfolio of Kenneth Beartusk. Stay Tuned ;)</Panel.Body>
+      </Panel>
+    </Col>
+    <Col sm={12} md={6}>
+      <Panel bsStyle="info" style={{height: 500}}>
+        <Panel.Heading>
+          <Panel.Title componentClass="h3">Future Content</Panel.Title>
+        </Panel.Heading>
+        <Panel.Body>There's going to be some serious kick ass content here..... soon!!!</Panel.Body>
+      </Panel>
+    </Col>
+    </div>
+  )
+}
+export default Begin
---
+++ app/index.js
@@ -0,0 +1,4 @@
+import Begin from '../app/components/main'
+import React from 'react'
+import {render} from 'react-dom'
+render(<Begin />, document.getElementById('app'))
