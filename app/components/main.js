57b8919aff8e2cadcb0556547608feddf5e9630f
Kenneth <kenneth.beartusk@gmail.com>
2018-03-11 01:08:17 -0700

    Commits between local and server due to Windows-Linux incompatibility

--- app/components/main.js
+++ app/components/main.js
@@ -2,40 +2,14 @@ import React from 'react'
 import {Panel, Col} from 'react-bootstrap'
 function Begin () {
   return (
-    <div style={{padding: "20px 20px 20px 20px", backgroundImage: `url("/assets/img/gifthitch.jpg")`}}>
+    <div style={{paddingTop: "100px", backgroundSize: "cover", backgroundImage: `url("/assets/img/gifthitch.jpg")`}}>
       <div className="row">
-        <Col sm={12} md={6}>
+        <Col sm={12} md={6} offset={{md:3}}>
           <Panel bsStyle="primary" style={{height: 500}}>
-        	<Panel.Heading>
-          	  <Panel.Title componentClass="h3">New Beginnings</Panel.Title>
-        	</Panel.Heading>
+          	<Panel.Heading>
+            	  <Panel.Title componentClass="h3">New Beginnings</Panel.Title>
+          	</Panel.Heading>
            	<Panel.Body style={{backgroundColor: "black"}}>This will be a website that displays the working portfolio of Kenneth Beartusk. Stay Tuned ;)</Panel.Body>
-          </Panel>
-        </Col>
-        <Col sm={12} md={6}>
-          <Panel bsStyle="info" style={{height: 500}}>
-            <Panel.Heading>
-              <Panel.Title componentClass="h3">Future Content</Panel.Title>
-            </Panel.Heading>
-            <Panel.Body>Theres going to be some serious kick ass content here..... soon!!!</Panel.Body>
-          </Panel>
-        </Col>
-      </div>
-      <div className="row">
-        <Col sm={12} md={6}>
-          <Panel bsStyle="primary" style={{height: 500}}>
-        	<Panel.Heading>
-          	  <Panel.Title componentClass="h3">New Beginnings</Panel.Title>
-        	</Panel.Heading>
-           	<Panel.Body>This will be a website that displays the working portfolio of Kenneth Beartusk. Stay Tuned ;)</Panel.Body>
-          </Panel>
-        </Col>
-        <Col sm={12} md={6}>
-          <Panel bsStyle="info" style={{height: 500}}>
-            <Panel.Heading>
-              <Panel.Title componentClass="h3">Future Content</Panel.Title>
-            </Panel.Heading>
-            <Panel.Body>Theres going to be some serious kick ass content here..... soon!!!</Panel.Body>
           </Panel>
         </Col>
       </div>
@@ -43,3 +17,10 @@ function Begin () {
   )
 }
 export default Begin
+/*
+  <Panel bsStyle="primary" style={{height: 500}}>
+    <Panel.Heading>
+        <Panel.Title componentClass="h3">New Beginnings</Panel.Title>
+    </Panel.Heading>
+    <Panel.Body style={{backgroundColor: "black"}}>This will be a website that displays the working portfolio of Kenneth Beartusk. Stay Tuned ;)</Panel.Body>
+  </Panel>*/
