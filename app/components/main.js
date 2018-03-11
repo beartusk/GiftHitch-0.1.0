c72b1d29c27e5f0616c636a87294d69b57338ffd
Kenneth <kenneth.beartusk@gmail.com>
2018-03-10 20:22:41 -0700

    j

--- app/components/main.js
+++ app/components/main.js
@@ -2,8 +2,8 @@ import React from 'react'
 import {Panel, Col} from 'react-bootstrap'
 function Begin () {
   return (
-    <div>
-      <div><i style={{backgroundColor: "#FFFFFF"}}>;ljfg;ladf;ldkjas;flkj</i></div>
+    <div style={{padding: "50px 50px 50px 50px"}>
+      <div style={{height: 50, backgroundColor: "#FFFFFF"}><i}>;ljfg;ladf;ldkjas;flkj</i></div>
       <div className="row" style={{backgroundColor: "black"}}>
         <Col sm={12} md={6}>
           <Panel bsStyle="primary" style={{height: 500}}>
