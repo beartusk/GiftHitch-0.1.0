e9ef9b42218e8df615627eb6a41bea514f399851
Kenneth <kenneth.beartusk@gmail.com>
2018-03-11 13:40:30 -0600

    Commits between local and server due to Windows-Linux incompatibility

--- app/components/main.js
+++ app/components/main.js
@@ -39,7 +39,22 @@ export default class Begin extends React.Component {
   render () {
     return (
       <div>
-        <p>nudasondsaf</p>
+        <div style={{paddingTop: "100px", backgroundSize: "cover", backgroundImage: `url("/assets/img/gifthitch.jpg")`}}>
+          <div className="row">
+            <Container>
+              <Col sm={12} md={6} offset={{md:3}}>
+                <Panel bsStyle="primary" style={{height: 500}}>
+                  <Panel.Heading>
+                      <Panel.Title componentClass="h3">New Beginnings</Panel.Title>
+                  </Panel.Heading>
+                  <Panel.Body style={{backgroundColor: "black"}}>This will be a website that displays the working portfolio of Kenneth Beartusk. Stay Tuned ;)</Panel.Body>
+                </Panel>
+              </Col>
+            </Container>
+          </div>
+        </div>
+        <div style={{height: "200px", backgroundColor: "black"}}>
+        </div>
       </div>
     )
   }
