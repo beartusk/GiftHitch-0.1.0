b5304f26b2045895a8f31db789113b177ad6f24f
Kenneth <kenneth.beartusk@gmail.com>
2018-03-11 15:59:04 -0600

    Commits between local and server due to Windows-Linux incompatibility

--- app/components/main.js
+++ app/components/main.js
@@ -75,28 +75,30 @@ export default class Begin extends React.Component {
     }
 
     return (
-        <div style={{padding: "100px 100px", height: 1000, backgroundSize: "cover", backgroundImage: `url("/assets/img/gifthitch.jpg")`}}>
+      <div>
+        <div style={{paddingTop: "100px", height: 1000, backgroundSize: "cover", backgroundImage: `url("/assets/img/gifthitch.jpg")`}}>
           {content}
         </div>
-        <Footer copyrights="&copy 2015 Copyright Text"
+        <Footer copyrights="copy 2015 Copyright Text"
           moreLinks={
-            <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+            <a className="white-text text-lighten-4 right" href="#!">More Links</a>
           }
           links={
             <ul>
-              <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
-              <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
-              <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
-              <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
+              <li><a className="white-text text-lighten-3" href="#!">Link 1</a></li>
+              <li><a className="white-text text-lighten-3" href="#!">Link 2</a></li>
+              <li><a className="white-text text-lighten-3" href="#!">Link 3</a></li>
+              <li><a className="white-text text-lighten-3" href="#!">Link 4</a></li>
             </ul>
           }
           style={{
-            backgroundColor: "black", fontColor: "white"
+            backgroundColor: "black"
           }}
         >
             <h5 className="white-text">Footer Content</h5>
-            <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+            <p className="white-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
         </Footer>;
+      </div>
     )
   }
 }
