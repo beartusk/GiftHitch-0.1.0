a8f755eb3102415be7f75eda87bd1fed72cfc03b
Kenneth <kenneth.beartusk@gmail.com>
2018-03-11 01:21:41 -0700

    Commits between local and server due to Windows-Linux incompatibility

--- app/components/main.js
+++ app/components/main.js
@@ -1,18 +1,34 @@
 import React from 'react'
 import {Panel, Col} from 'react-bootstrap'
+
+intro(){
+  return(
+    <Col sm={12} md={6} offset={{ md:3 }}>
+      <Panel bsStyle="primary" style={{height: 500}}>
+        <Panel.Heading>
+            <Panel.Title componentClass="h3">New Beginnings</Panel.Title>
+        </Panel.Heading>
+        <Panel.Body style={{backgroundColor: "black"}}>This will be a website that displays the working portfolio of Kenneth Beartusk. Stay Tuned ;)</Panel.Body>
+      </Panel>
+    </Col>
+  )
+}
+
 function Begin () {
   return (
     <div>
       <div style={{paddingTop: "100px", backgroundSize: "cover", backgroundImage: `url("/assets/img/gifthitch.jpg")`}}>
         <div className="row">
-          <Col sm={12} md={6} offset={{md:3}}>
-            <Panel bsStyle="primary" style={{height: 500}}>
-            	<Panel.Heading>
-              	  <Panel.Title componentClass="h3">New Beginnings</Panel.Title>
-            	</Panel.Heading>
-             	<Panel.Body style={{backgroundColor: "black"}}>This will be a website that displays the working portfolio of Kenneth Beartusk. Stay Tuned ;)</Panel.Body>
-            </Panel>
-          </Col>
+          <Container>
+            <Col sm={12} md={6} offset={{md:3}}>
+              <Panel bsStyle="primary" style={{height: 500}}>
+              	<Panel.Heading>
+                	  <Panel.Title componentClass="h3">New Beginnings</Panel.Title>
+              	</Panel.Heading>
+               	<Panel.Body style={{backgroundColor: "black"}}>This will be a website that displays the working portfolio of Kenneth Beartusk. Stay Tuned ;)</Panel.Body>
+              </Panel>
+            </Col>
+          </Container>
         </div>
       </div>
       <div style={{height: "200px", backgroundColor: "black"}}>
