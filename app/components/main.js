b1784db937b44a156ae327ace7de56c3c51cf66d
Kenneth <kenneth.beartusk@gmail.com>
2018-03-10 18:52:34 -0700

    j

--- app/components/main.js
+++ app/components/main.js
@@ -2,24 +2,7 @@ import React from 'react'
 import {Panel, Col} from 'react-bootstrap'
 function Begin () {
   return (
-    <div className="row">
-      <Col sm={12} md={6}>
-        <Panel bsStyle="primary" style={{height: 500}}>
-      	<Panel.Heading>
-        	  <Panel.Title componentClass="h3">New Beginnings</Panel.Title>
-      	</Panel.Heading>
-         	<Panel.Body>This will be a website that displays the working portfolio of Kenneth Beartusk. Stay Tuned ;)</Panel.Body>
-        </Panel>
-      </Col>
-      <Col sm={12} md={6}>
-        <Panel bsStyle="info" style={{height: 500}}>
-          <Panel.Heading>
-            <Panel.Title componentClass="h3">Future Content</Panel.Title>
-          </Panel.Heading>
-          <Panel.Body>Theres going to be some serious kick ass content here..... soon!!!</Panel.Body>
-        </Panel>
-      </Col>
-    </div>
+    <div>
       <div className="row">
         <Col sm={12} md={6}>
           <Panel bsStyle="primary" style={{height: 500}}>
@@ -38,6 +21,25 @@ function Begin () {
           </Panel>
         </Col>
       </div>
+      <div className="row">
+        <Col sm={12} md={6}>
+          <Panel bsStyle="primary" style={{height: 500}}>
+        	<Panel.Heading>
+          	  <Panel.Title componentClass="h3">New Beginnings</Panel.Title>
+        	</Panel.Heading>
+           	<Panel.Body>This will be a website that displays the working portfolio of Kenneth Beartusk. Stay Tuned ;)</Panel.Body>
+          </Panel>
+        </Col>
+        <Col sm={12} md={6}>
+          <Panel bsStyle="info" style={{height: 500}}>
+            <Panel.Heading>
+              <Panel.Title componentClass="h3">Future Content</Panel.Title>
+            </Panel.Heading>
+            <Panel.Body>Theres going to be some serious kick ass content here..... soon!!!</Panel.Body>
+          </Panel>
+        </Col>
+      </div>
+    </div>
   )
 }
 export default Begin
