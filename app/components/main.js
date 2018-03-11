d7e3c87fdb2f0e05a1c200ec9ad16595b812a3fa
Kenneth <kenneth.beartusk@gmail.com>
2018-03-11 01:30:31 -0700

    Commits between local and server due to Windows-Linux incompatibility

--- app/components/main.js
+++ app/components/main.js
@@ -1,42 +1,46 @@
 import React from 'react'
 import {Panel, Col, Container} from 'react-bootstrap'
 
-function intro () {
-  return(
-    <Col sm={12} md={6} offset={{ md:3 }}>
-      <Panel bsStyle="primary" style={{height: 500}}>
-        <Panel.Heading>
-            <Panel.Title componentClass="h3">New Beginnings</Panel.Title>
-        </Panel.Heading>
-        <Panel.Body style={{backgroundColor: "black"}}>This will be a website that displays the working portfolio of Kenneth Beartusk. Stay Tuned ;)</Panel.Body>
-      </Panel>
-    </Col>
-  )
-}
+export default class Begin extends React.component {
 
-function Begin () {
-  return (
-    <div>
-      <div style={{paddingTop: "100px", backgroundSize: "cover", backgroundImage: `url("/assets/img/gifthitch.jpg")`}}>
-        <div className="row">
-          <Container>
-            <Col sm={12} md={6} offset={{md:3}}>
-              <Panel bsStyle="primary" style={{height: 500}}>
-              	<Panel.Heading>
-                	  <Panel.Title componentClass="h3">New Beginnings</Panel.Title>
-              	</Panel.Heading>
-               	<Panel.Body style={{backgroundColor: "black"}}>This will be a website that displays the working portfolio of Kenneth Beartusk. Stay Tuned ;)</Panel.Body>
-              </Panel>
-            </Col>
-          </Container>
+  constructor(props, context){}
+
+  intro () {
+    return(
+      <Col sm={12} md={6} offset={{ md:3 }}>
+        <Panel bsStyle="primary" style={{height: 500}}>
+          <Panel.Heading>
+              <Panel.Title componentClass="h3">New Beginnings</Panel.Title>
+          </Panel.Heading>
+          <Panel.Body style={{backgroundColor: "black"}}>This will be a website that displays the working portfolio of Kenneth Beartusk. Stay Tuned ;)</Panel.Body>
+        </Panel>
+      </Col>
+    )
+  }
+
+  render () {
+    return (
+      <div>
+        <div style={{paddingTop: "100px", backgroundSize: "cover", backgroundImage: `url("/assets/img/gifthitch.jpg")`}}>
+          <div className="row">
+            <Container>
+              <Col sm={12} md={6} offset={{md:3}}>
+                <Panel bsStyle="primary" style={{height: 500}}>
+                	<Panel.Heading>
+                  	  <Panel.Title componentClass="h3">New Beginnings</Panel.Title>
+                	</Panel.Heading>
+                 	<Panel.Body style={{backgroundColor: "black"}}>This will be a website that displays the working portfolio of Kenneth Beartusk. Stay Tuned ;)</Panel.Body>
+                </Panel>
+              </Col>
+            </Container>
+          </div>
+        </div>
+        <div style={{height: "200px", backgroundColor: "black"}}>
         </div>
       </div>
-      <div style={{height: "200px", backgroundColor: "black"}}>
-      </div>
-    </div>
-  )
+    )
+  }
 }
-export default Begin
 /*
   <Panel bsStyle="primary" style={{height: 500}}>
     <Panel.Heading>
