a69db77e3f3675930dc241ccfc6d42c873bdb25c
Kenneth <kenneth.beartusk@gmail.com>
2018-03-11 14:12:23 -0600

    Commits between local and server due to Windows-Linux incompatibility

--- app/components/main.js
+++ app/components/main.js
@@ -18,39 +18,50 @@ export default class Begin extends React.Component {
     super(props, context);
 
     this.state = {
-      button: false
+      introButtonPressed: false
     }
   }
-  /*
   intro () {
     return(
-      <Col sm={12} md={6} offset={{ md:3 }}>
-        <Panel bsStyle="primary" style={{height: 500}}>
-          <Panel.Heading>
-              <Panel.Title componentClass="h3">New Beginnings</Panel.Title>
-          </Panel.Heading>
-          <Panel.Body style={{backgroundColor: "black"}}>This will be a website that displays the working portfolio of Kenneth Beartusk. Stay Tuned ;)</Panel.Body>
-        </Panel>
-      </Col>
+      <Grid>
+        <Row>
+          <Col sm={12} md={6} offset={{ md:3 }}>
+            <Panel bsStyle="primary" style={{height: 500}}>
+              <Panel.Heading>
+                  <Panel.Title componentClass="h3">New Beginnings</Panel.Title>
+              </Panel.Heading>
+              <Panel.Body style={{backgroundColor: "black"}}>This will be a website that displays the working portfolio of Kenneth Beartusk. Stay Tuned ;)</Panel.Body>
+            </Panel>
+          </Col>
+        </Row>
+      </Grid>
     )
   }
-*/
+
+  introButtonPressed(){
+    this.setState({introButtonPressed: true});
+  }
+
+  introButton(){
+    return(
+      <Button bsStyle="primary" bsSize="large" block onClick={this.introButtonPressed.bind(this)}>
+        Calculate Rates
+      </Button>
+    )
+  }
+
   render () {
+    if(this.state.introButtonPressed == true){
+      content = this.intro();
+    }
+    else{
+      content = this.introButton();
+    }
+
     return (
       <div>
         <div style={{paddingTop: "100px", backgroundSize: "cover", backgroundImage: `url("/assets/img/gifthitch.jpg")`}}>
-          <Grid>
-            <Row>
-              <Col sm={12} md={6} mdOffset={3}>
-                <Panel bsStyle="primary" style={{height: 500}}>
-                  <Panel.Heading>
-                      <Panel.Title componentClass="h3">New Beginnings</Panel.Title>
-                  </Panel.Heading>
-                  <Panel.Body style={{backgroundColor: "black"}}>This will be a website that displays the working portfolio of Kenneth Beartusk. Stay Tuned ;)</Panel.Body>
-                </Panel>
-              </Col>
-            </Row>
-          </Grid>
+
         </div>
         <div style={{height: "200px", backgroundColor: "black"}}>
         </div>
@@ -58,34 +69,3 @@ export default class Begin extends React.Component {
     )
   }
 }
-/*
-  <Panel bsStyle="primary" style={{height: 500}}>
-    <Panel.Heading>
-        <Panel.Title componentClass="h3">New Beginnings</Panel.Title>
-    </Panel.Heading>
-    <Panel.Body style={{backgroundColor: "black"}}>This will be a website that displays the working portfolio of Kenneth Beartusk. Stay Tuned ;)</Panel.Body>
-  </Panel>*/
-
-  /*
-  render () {
-    return (
-      <div>
-        <div style={{paddingTop: "100px", backgroundSize: "cover", backgroundImage: `url("/assets/img/gifthitch.jpg")`}}>
-          <div className="row">
-            <Container>
-              <Col sm={12} md={6} offset={{md:3}}>
-                <Panel bsStyle="primary" style={{height: 500}}>
-                  <Panel.Heading>
-                      <Panel.Title componentClass="h3">New Beginnings</Panel.Title>
-                  </Panel.Heading>
-                  <Panel.Body style={{backgroundColor: "black"}}>This will be a website that displays the working portfolio of Kenneth Beartusk. Stay Tuned ;)</Panel.Body>
-                </Panel>
-              </Col>
-            </Container>
-          </div>
-        </div>
-        <div style={{height: "200px", backgroundColor: "black"}}>
-        </div>
-      </div>
-    )
-  }*/
