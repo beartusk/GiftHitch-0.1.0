21920bc6df5b5ec9d8a63ac3676794f8eebba844
Kenneth <kenneth.beartusk@gmail.com>
2018-07-25 11:58:12 -0600

    first new componenets from feeaudit

--- app/components/main.js
+++ app/components/main.js
@@ -1,3 +1,7 @@
+
+import $ from 'jquery'
+import _ from 'lodash'
+
 import React from 'react'
 import {
   Table,
@@ -9,18 +13,67 @@ import {
   Panel,
   DropdownButton,
   MenuItem,
-  Well
+  Well,
+  Navbar,
+  Nav,
+  NavItem,
+  NavDropdown
 } from 'react-bootstrap'
 import {Footer} from 'react-materialize'
 
-var buttonStyle = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)"
+import Products from "./products"
+
+let styles = {
+  imageContainer: {
+    maxHeight: 375,
+    background: "rgba(255,255,255,0.85)",
+    boxShadow: "0px 5px 16px 0px rgba(0,0,0,1)",
+  },
+  image: {
+    height: "auto",
+    width: "90%",
+    marginLeft: "auto",
+    marginRight: "auto",
+    display: "block",
+    borderTop: "1px solid black",
+    //borderBottom: "1px solid black",
+  },
+  imageTransition: {
+    overflow: "hidden",
+    transform: "translateY(0)",
+    transition: "transform 1.5s linear"
+  },
+  fade: {
+    zIndex: "1",
+    height: "100%",
+    width: "100%",
+    position: "absolute",
+    background: "linear-gradient(to right, rgba(255,255,255,1) 5%, rgba(255,255,255,0) 15%, rgba(255,255,255,0) 85%, rgba(255,255,255,1) 95%)"
+  },
+  fade1: {
+    zIndex: "1",
+    height: "100%",
+    width: "15%",
+    position: "absolute",
+    background: "linear-gradient(to right, rgba(255,255,255,1) 33%, rgba(255,255,255,0))"
+  },
+  fade2: {
+    marginLeft: "85%",
+    zIndex: "1",
+    height: "100%",
+    width: "15%",
+    position: "absolute",
+    background: "linear-gradient(to right, rgba(255,255,255,0), rgba(255,255,255,1) 67%)"
+  },
+  buttonStyle = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)"
+  }
 }
 
-export default class Begin extends React.Component {
+export default class Main extends React.Component {
 
   constructor(props, context){
     super(props, context);
@@ -28,46 +81,6 @@ export default class Begin extends React.Component {
     this.state = {
       introButtonPressed: false
     }
-  }
-
-  intro () {
-    return(
-      <Grid>
-        <Row>
-          <Col sm={10} md={6} mdOffset={3} smOffset={1}>
-            <Panel bsStyle="primary" style={{height: 500, backgroundColor: "black", borderColor: "white", fontColor: "white"}}>
-              <Panel.Heading style={{backgroundColor: "black", borderColor: "white", fontColor: "white"}}>
-                  <Panel.Title componentClass="h3">Gift Hitch Biatch!!!!</Panel.Title>
-              </Panel.Heading>
-              <Panel.Body style={{backgroundColor: "black", borderColor: "white", fontColor: "white"}}>
-                <h4 style={{ fontColor: "white"}}>This will be the website that displays Anything and everything GiftHitch!!!</h4>
-                <h4 style={{ fontColor: "white"}}>We can put whatever the fuck you want here bro. And, this is mostly HTML with some modern Javascript Framework stuff. It is called React JS and it is what Facebook and a bunch of other sites use.</h4>
-                <h4 style={{ fontColor: "white"}}>With your html background you could catch on to this pretty quickly</h4>
-                <h4 style={{ fontColor: "white"}}>Text is supposed to be white will fix later</h4>
-              </Panel.Body>
-            </Panel>
-          </Col>
-        </Row>
-      </Grid>
-    )
-  }
-
-  introButtonPressed(){
-    this.setState({introButtonPressed: true});
-  }
-
-  introButton(){
-    return(
-      <Grid>
-        <Row>
-          <Col sm={10} md={2} smOffset={1} mdOffset={5}>
-            <Button bsStyle="primary" bsSize="large" block onClick={this.introButtonPressed.bind(this)} style={{backgroundColor: "black", borderColor: "white", fontColor: "white"}}>
-              What is GiftHitch?
-            </Button>
-          </Col>
-        </Row>
-      </Grid>
-    )
   }
 
   render () {
@@ -80,29 +93,84 @@ export default class Begin extends React.Component {
     }
 
     return (
-      <div>
-        <div style={{paddingTop: "100px", height: 1000, backgroundSize: "cover", backgroundImage: `url("/assets/img/gifthitch.jpg")`}}>
-          {content}
+      <div style={{position: "relative", width: "100%"}}>
+        <div>
+          <div style={{maxHeight: 65, height: 65, backgroundColor: "white", position: "relative", zIndex: "2"}}>
+            <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8 offset-lg-1">
+              <div style={{marginRight: 15, marginTop: 3, display: "inline-block", float: "left"}}>
+                <p style={{fontFamily: "Century", fontSize: 35, position: "relative"}}>Printing For Less</p>
+              </div>
+              <div style={{width: 1, height: 65, borderLeft: "1px inset black", marginRight: 25, display: "inline-block", float: "left"}}></div>
+              <a href="https://www.printingforless.com/"  target = "_blank" style={{fontSize: 16, marginTop: 23, marginRight: 25, display: "inline-block", float: "left"}}>Home</a>
+              <a href="https://www.printingforless.com/Business-Printing-Services.html"  target = "_blank" style={{fontSize: 16, marginTop: 23, marginRight: 25, display: "inline-block", float: "left"}}>Business Services</a>
+              <a href="https://www.printingforless.com/About-PFL.html"  target = "_blank" style={{fontSize: 16, marginTop: 23, marginRight: 25, display: "inline-block", float: "left"}}>About Us</a>
+              <a href="https://mypfl.com/login.aspx?ReturnUrl=%2f"  target = "_blank" style={{fontSize: 16, marginTop: 23, marginRight: 25, display: "inline-block", float: "left"}}>MyPFL</a>
+            </div>
+            <div className="col-xs-4 col-sm-4 col-md-4 col-lg-2" style={{}}>
+              <a style={{marginTop: 8, display: "inline-block", float: "right"}} href="http://www.bbb.org/eastern-washington/business-reviews/credit-card-processing-service/frontline-processing-corporation-in-bozeman-mt-64004904/#bbbonlineclick" target="_blank" rel="nofollow">
+                <img style={{width: 130}} src="/assets/img/ab-seal-horizontal-black-large.jpg" alt="Frontline Processing Corporation BBB Business Review" />
+              </a>
+            </div>
+          </div>
+          <div style={styles.imageContainer}>
+            <img src="/assets/img/vendor-pfl.png" style={styles.image} />
+          </div>
+          <FeeAuditCalc />
         </div>
-        <Footer copyrights="copy 2015 Copyright Text"
-          moreLinks={
-            <a className="white-text text-lighten-4 right" href="#!">More Links</a>
-          }
-          links={
-            <ul>
-              <li><a className="white-text text-lighten-3" href="#!">Facebook Link</a></li>
-              <li><a className="white-text text-lighten-3" href="#!">Twitter Link</a></li>
-              <li><a className="white-text text-lighten-3" href="#!">Youtube Link</a></li>
-              <li><a className="white-text text-lighten-3" href="#!">Link 4</a></li>
-            </ul>
-          }
-          style={{
-            backgroundColor: "black"
-          }}
-        >
-            <h5 className="white-text">Footer Content</h5>
-            <p className="white-text">GiftHitch blahblahblah.</p>
-        </Footer>
+        <div style={{backgroundColor: "#F8F8F8F8", position: "absolute", bottom: 0, width: "100%", zIndex: "2", borderTop: "1px solid rgba(0,0,0,0.8)"}}>
+            <div className="col-xs-6 col-sm-6 col-md-6 col-lg-3 offset-lg-2">
+              <div className="col-xs-9 col-sm-9 col-md-9 col-lg-9">
+                <a title = "Payment Portal" href = "https://www.paymentportal.us" target = "_blank">
+                  <img className="pp-logo-long" src="/assets/img/portal-alpha-longcopy.png" alt = "Payment Portal" />
+                </a>
+                <p style={{textAlign: "center"}}>© 2018 Frontline Processing</p>
+              </div>
+              <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                <br />
+                <a href="http://www.bbb.org/eastern-washington/business-reviews/credit-card-processing-service/frontline-processing-corporation-in-bozeman-mt-64004904/#bbbonlineclick" target="_blank" rel="nofollow">
+                  <img style={{width: 140}} src="https://seal-spokane.bbb.org/seals/black-seal-187-130-bbb-64004904.png" alt="Frontline Processing Corporation BBB Business Review" />
+                </a>
+                <br />
+                <br />
+              </div>
+            </div>
+            <div className="col-xs-3 col-sm-3 col-md-3 col-lg-2">
+              <br />
+              <div className="row">
+                <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3 offset-xs-2 offset-sm-2 offset-md-2 offset-lg-2">
+                  <a title="Twitter" href="https://twitter.com/FPC_Payments" target = "_blank">
+                    <img alt="Twitter" src="https://s3-us-west-2.amazonaws.com/pp-logo/twitter-icon-32.png" />
+                  </a>
+                  <br />
+                  <br />
+                  <a title="Facebook" href="http://facebook.com/pages/Frontline-Processing/330283311741"  target = "_blank">
+                    <img alt="Facebook" src="https://s3-us-west-2.amazonaws.com/pp-logo/facebook-icon-32.png"  />
+                  </a>
+                </div>
+                <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                  <a title ="Frontline Proessing on Instagram" href = "http://instagram.com/frontlineprocessing"  target = "_blank">
+                    <img src="https://s3-us-west-2.amazonaws.com/pp-logo/instagram-icon-32.png" alt ="FLP on Instagram" />
+                  </a>
+                  <br />
+                  <br />
+                  <a title="Youtube" href="https://www.youtube.com/channel/UC6tEPWTqOzixKQpbE8b5YMg"  target = "_blank">
+                    <img alt="Youtube" src="https://s3-us-west-2.amazonaws.com/pp-logo/youtube-icon-32.png"  />
+                  </a>
+                </div>
+                <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                  <a title = "Frontline Processing on LinkedIn" href = "https://www.linkedin.com/in/frontlineprocessing" target = "_blank">
+                    <img src="https://s3-us-west-2.amazonaws.com/pp-logo/linkedin-icon-32.png" alt = "Frontline Processing on Linked in" />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+              <br />
+              <p>Contact Support</p>
+              <p>Phone: (866) 651-3068</p>
+              <p style={{wordWrap: "break-word"}}>Email: customerservice@frontlineprocessing.com</p>
+            </div>
+          </div>
       </div>
     )
   }
---
+++ app/components/products.js
@@ -0,0 +1,313 @@
+import $ from 'jquery'
+import _ from 'lodash'
+import '../../node_modules/bootstrap/dist/css/bootstrap-grid.css'
+import '../assets/css/your_style.css'
+
+import publicIp from 'public-ip'
+import React from 'react'
+import {
+  Table,
+  Button,
+  ButtonGroup,
+  Grid,
+  Row,
+  Col,
+  Panel,
+  DropdownButton,
+  MenuItem,
+  Well,
+  Popover,
+  OverlayTrigger
+} from 'react-bootstrap'
+
+let mainLabel = {
+  fontWeight: "bold",
+  fontFamily: "Abel",
+  fontSize: 22,
+  marginBottom: 14
+}
+let inputStyle = {
+  height: 40,
+  width: 400,
+  fontFamily: "Abel",
+  fontSize: 20,
+  paddingLeft: 24,
+  paddingRight: 0,
+  marginBottom: 24,
+  display: "inline-block",
+  float: "left"
+}
+let emailInputStyle = {
+  height: 40,
+  width: 400,
+  fontFamily: "Abel",
+  fontSize: 20,
+  marginBottom: 24,
+  display: "inline-block",
+  float: "left"
+}
+let dollarStyle = {
+  position: "absolute",
+  display: "block",
+  transform: "translate(0, 10%)",
+  top: 2,
+  pointerEvents: "none",
+  width: 28,
+  textAlign: "center",
+  fontStyle: "normal",
+  fontFamily: "Abel",
+  fontSize: 20
+}
+
+export default class Products extends React.Component {
+
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      showRate: false,
+      shiftedPage: false,
+      hover1: false,
+      hover2: false,
+      hover3: false,
+      hover4: false,
+      mainBackgroundHeight: 150,
+      mainHeight: 1700,
+      validProcessingInput: null,
+      BusinessType: 0,
+      MonthlySales: 0,
+      AverageTicket: 0,
+      CurRateState: 0,
+      CurMonthState: 0,
+      CurAnnualState: 0,
+      NewRateState1: 0,
+      NewRateState2: 0,
+      NewAnnualState: 0,
+      NewMonthState: 0,
+      SaveRateState: 0,
+      SaveMonthState: 0,
+      SaveAnnualState: 0,
+      SaveTriAnnualState: 0,
+      executed: null,
+      details: {
+        ip: null,
+        email: null,
+        terms: null
+      },
+      confirmDisabled: false
+    };
+
+  }
+
+
+  componentDidMount(){
+    if(screen.width < 600){
+      this.setState({mainHeight: screen.height*2.4})
+    }
+  }
+
+
+
+
+  _setService(idVal, val1)
+  {
+    document.getElementById(idVal).value = val1;
+    this.setState({serviceType: val1});
+  }
+
+  _setVal(idVal, val1)
+  {
+    document.getElementById(idVal).value = val1;
+    document.getElementById(idVal+'Val').innerHTML = val1;
+  }
+
+  toggleHover1(){
+    this.setState({hover1: !this.state.hover1})
+  }
+
+  toggleHover2(){
+    this.setState({hover2: !this.state.hover2})
+  }
+
+  toggleHover3(){
+    this.setState({hover3: !this.state.hover3})
+  }
+
+  toggleHover4(){
+    this.setState({hover4: !this.state.hover4})
+  }
+
+  componentDidUpdate(prevProps, prevState){
+  }
+
+
+  render() {
+
+    let mainStyle = {
+      minHeight: this.state.mainHeight,
+      background: "rgba(255,255,255,0.85)",
+      paddingBottom: 50,
+      //zIndex: "0"
+    };
+    let headerLabel ={
+       fontSize: "38px",
+       textAlign: "center",
+       fontFamily: "Abel"
+    }
+    let secondaryLabel = {
+      fontFamily: "Abel",
+      fontSize: 18,
+      marginBottom: 14
+    }
+    let wellStyle = {
+      fontFamily: "Abel",
+      fontSize: 20,
+      marginBottom: 15,
+      marginTop: 15,
+      marginLeft: 15,
+      marginRight: 15
+    }
+
+
+    let ratesBoxStyle = {zIndex: 2, paddingBottom: 20, backgroundColor: "rgba(255,255,255,0)"};
+    let descriptionBoxStyle = {
+      paddingTop: null,
+      paddingBottom: null,
+      paddingLeft: null,
+      paddingRight: null,
+      boxShadow: null,
+    };
+    let calcStyle  = {
+      paddingLeft: null,
+      boxShadow: null,
+      paddingTop: null,
+    };
+    let adBoxStyle = {
+      boxShadow: null
+    };
+
+    if(this.state.hover2 == true){
+      descriptionBoxStyle = {
+        paddingTop: 1,
+        paddingBottom: 30,
+        paddingLeft: 60,
+        paddingRight: 60,
+        boxShadow: "0px -15px 16px -15px rgba(0,0,0,0.8), 0px 15px 16px -15px rgba(0,0,0,0.8)"
+      }
+      calcStyle = {
+        boxShadow: "6px 6px 16px -5px rgba(0,0,0,0.4)",
+        paddingTop: 15
+      }
+      adBoxStyle.boxShadow = ""
+    }
+    else{
+      descriptionBoxStyle = {
+        paddingTop: 1,
+        paddingBottom: 30,
+        paddingLeft: 60,
+        paddingRight: 60,
+        boxShadow: "0px -10px 16px -10px rgba(0,0,0,0.4)"
+      }
+      calcStyle.boxShadow = "6px -6px 16px -5px rgba(0,0,0,0.4)"
+      adBoxStyle.boxShadow = "0px -6px 16px -5px rgba(0,0,0,0.4)"
+
+      if(this.state.hover3 == true){
+        calcStyle = {
+          boxShadow: "6px -6px 16px -5px rgba(0,0,0,0.8)",
+          paddingTop: 15
+        }
+      }
+      else{
+        calcStyle = {
+          boxShadow: "6px -6px 16px -5px rgba(0,0,0,0.4)",
+          paddingTop: 15
+        }
+      }
+      if(this.state.hover4 == true){
+        adBoxStyle = {
+          boxShadow: "-6px -6px 16px -5px rgba(0,0,0,0.8)"
+        }
+      }
+      else{
+        adBoxStyle = {
+          boxShadow: "-4px -10px 16px -10px rgba(0,0,0,0.4)"
+        }
+      }
+    }
+
+    let welcomeString =
+      "What most people don't know is that up to half of their fees each month are to pay sales commissions and other expenses that do not add value to your processing experiences.";
+
+
+
+    return (
+      <div style={mainStyle}>
+        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12" style={{
+          marginTop: 5,
+          marginBottom: 20,
+        }}>
+          <div className="col-xs-12 col-sm-12 col-md-12 col-lg-10 offset-lg-1">
+            <div style={{margin: "0 auto", width: 1250}}>
+              <h1 style={headerLabel}>
+                Compare your rates to ours and see how much you can save!
+              </h1>
+            </div>
+          </div>
+        </div>
+        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12" style={ratesBoxStyle}>
+          <div className="row" style={{marginBottom: 100}}>
+            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-10 offset-lg-1">
+              <div className="row">
+                <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12" style={descriptionBoxStyle}  onMouseEnter={this.toggleHover2.bind(this)} onMouseLeave={this.toggleHover2.bind(this)}>
+                  <div>
+                    <h1 style={{fontFamily: "Abel", fontSize: 18, fontWeight: "bold", textAlign: "center"}}>Welcome Letter</h1>
+                    <br />
+                    <div className="col-xs-8 col-sm-8 col-md-8 col-lg-7 offset-lg-1" style={{fontFamily: "Song Myung"}}>
+                      <p style={{fontSize: 16}}>
+                        Hi and welcome to Feeaudit.com a highly intuitive credit card processing rate calculator, my name is Chris Kittler and I am 35-year veteran of the card payment industry.
+                      </p>
+                      <p style={{fontSize: 16}}>
+                        While looking into mortgage costs I noticed that there are over 4.7 million loan calculators on the internet… and not one was
+                        a credit card processing rate calculator able to help me figure out what I am actually paying to take a credit-card payments…
+                        like the cost of a loan.  So, we decided to put one together that kept it simple.
+                      </p>
+                      <p style={{fontSize: 16}}>
+                        We took the four main issues that make up your rate; the type of business, the amount of volume, the average sale and the total
+                        fees you are charged.  Then we compare it to the best pricing we can offer through our service to give you a clear price comparison.
+                        Furthermore, without a sales-person we would pass those savings along to you.
+                      </p>
+                      <p style={{fontSize: 16}}>
+                        {welcomeString}
+                      </p>
+                      <p style={{fontSize: 16}}>
+                        By using FeeAudit you should see significant savings, but please take note, there are those that already have great rates will not see any change. Let’s make it clear, the point
+                        to FeeAudit is to give you, the user an easy to use tool to compare what you are paying to what you could be paying.
+                      </p>
+                      <p style={{fontSize: 16}}>
+                        In regard to promises, prior to any final calculations you will need to provide your processing statements that were used to complete the
+                        calculation. We want to assure that you put in the right data so you are guaranteed to get the rate that calculated.
+                      </p>
+                    </div>
+                    <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                      <img src="/assets/img/Chris cutout.png" style={{transform: "translateY(-185px)", position: "absolute", width: 420}} />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-xs-6 col-sm-6 col-md-6 col-lg-8" style={calcStyle} onMouseEnter={this.toggleHover3.bind(this)} onMouseLeave={this.toggleHover3.bind(this)}>
+                  <h1 style={{fontFamily: "Abel", fontSize: 32, fontWeight: "bold", textAlign: "center", marginBottom: 15}}>Rate Calculator</h1>
+
+                </div>
+                <div className="col-xs-6 col-sm-6 col-md-6 col-lg-4" style={adBoxStyle} onMouseEnter={this.toggleHover4.bind(this)} onMouseLeave={this.toggleHover4.bind(this)}>
+                  <h1 style={{fontFamily: "Abel", fontSize: 18, fontWeight: "bold", textAlign: "center"}}>Recommended</h1>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
